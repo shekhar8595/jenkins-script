61e6d97fcd92580b4b900ecd9af5f99806224f01
shekhar.sharma <shekhar.sharma@inadev.com>
2025-12-24 03:08:52 +0530

    provider updated

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 resource "google_compute_instance" "example-vm" {
-  name         = "devops-jenkins-webhook-freestyle-setup18"
+  name         = "devops-jenkins-webhook-freestyle-setup189"
   machine_type = "e2-medium"  # Choose an appropriate machine type
   zone         = var.zone  # Specify the zone
 
