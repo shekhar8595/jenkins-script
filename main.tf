454b19ab581e8ddafd920057e25607923456777a
shekhar.sharma <shekhar.sharma@inadev.com>
2025-12-24 01:33:37 +0530

    provider updated

--- main.tf
+++ main.tf
@@ -1,27 +1,27 @@
-resource "google_compute_instance" "example-vm" {
-  name         = "devops-vm-update-server-12"
-  machine_type = "e2-medium"  # Choose an appropriate machine type
-  zone         = var.zone  # Specify the zone
+# resource "google_compute_instance" "example-vm" {
+#   name         = "devops-vm-update-server-12"
+#   machine_type = "e2-medium"  # Choose an appropriate machine type
+#   zone         = var.zone  # Specify the zone
 
-  boot_disk {
-    initialize_params {
-      image = var.image  # Use a base image
-    }
-  }
-
-  network_interface {
-    network = "default"
-
-    access_config {
-      // Allocate a public IP address
-    }
-  }
-
-#   metadata = {
-#     ssh-keys = "user:${file("~/.ssh/id_rsa.pub")}"  # Provide your public SSH key for access
+#   boot_disk {
+#     initialize_params {
+#       image = var.image  # Use a base image
+#     }
 #   }
 
-  tags = ["web-server"]
-}
+#   network_interface {
+#     network = "default"
+
+#     access_config {
+#       // Allocate a public IP address
+#     }
+#   }
+
+# #   metadata = {
+# #     ssh-keys = "user:${file("~/.ssh/id_rsa.pub")}"  # Provide your public SSH key for access
+# #   }
+
+#   tags = ["web-server"]
+# }
 
 
